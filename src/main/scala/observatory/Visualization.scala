95e2ab5c9733058565409eddf3d6c1d5b32f8eb4
Francesco Trotta <fratrotta@gmail.com>
2017-03-30 20:44:52 +0200

    greatCircleDistance

--- src/main/scala/observatory/Visualization.scala
+++ src/main/scala/observatory/Visualization.scala
@@ -1,11 +1,40 @@
 package observatory
 
 import com.sksamuel.scrimage.{Image, Pixel}
+import java.lang.Math.{acos, sin, cos, PI}
 
 /**
   * 2nd milestone: basic visualization
   */
 object Visualization {
+
+  /* https://en.wikipedia.org/wiki/Great-circle_distance
+    R = 6371km (does it really care?)
+   */
+  val r = 1
+
+  def greatCircleDistance(a: Location, b: Location): Double = {
+    def rad(x: Double): Double = {
+      x  * (2*PI) / 360
+    }
+
+    def fi1 = rad(a.lat)
+    def lambda1 = rad(a.lon)
+    def fi2 = rad(b.lat)
+    def lambda2 = rad(b.lon)
+
+    r * (acos( sin(fi1) * sin(fi2) + cos(fi1) * cos(fi2) * cos(lambda1-lambda2))) // cosine is even
+  }
+
+  // https://en.wikipedia.org/wiki/Inverse_distance_weighting
+  def u(x: Location, temperatures: Iterable[(Location, Double)]): Double = {
+    def r(a: (Double, Double), e: (Location, Double)): (Double, Double) = {
+      val w = 1 / greatCircleDistance(x, e._1)
+      (w * e._2, w)
+    }
+    val (num, den) = temperatures.foldLeft((0.0, 0.0))(r)
+    num / den
+  }
 
   /**
     * @param temperatures Known temperatures: pairs containing a location and the temperature at this location
@@ -13,7 +42,12 @@ object Visualization {
     * @return The predicted temperature at `location`
     */
   def predictTemperature(temperatures: Iterable[(Location, Double)], location: Location): Double = {
-    ???
+    temperatures.find(_._1 == location) match {
+      case Some((_, t)) => t
+      case None => {
+        u(location, temperatures)
+      }
+    }
   }
 
   /**
--- src/test/scala/observatory/VisualizationTest.scala
+++ src/test/scala/observatory/VisualizationTest.scala
@@ -5,9 +5,18 @@ import org.junit.runner.RunWith
 import org.scalatest.FunSuite
 import org.scalatest.junit.JUnitRunner
 import org.scalatest.prop.Checkers
+import java.lang.Math.PI
+
+import Visualization._
 
 @RunWith(classOf[JUnitRunner])
 class VisualizationTest extends FunSuite with Checkers {
-
-
+  test("greatCircleDistance") {
+    // ENHANCE property based testing
+    assert(greatCircleDistance(Location(0,0), Location(0,180)) === PI * r)
+    assert(greatCircleDistance(Location(0,180), Location(0,0)) === greatCircleDistance(Location(0,0), Location(0,180)))
+    assert(greatCircleDistance(Location(0,0), Location(180,0)) === PI * r)
+    assert(greatCircleDistance(Location(0,0), Location(0,360)) === 0)
+    assert(greatCircleDistance(Location(90,0), Location(-90,0)) === PI * r)
+  }
 }
