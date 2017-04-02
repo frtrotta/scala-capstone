e899537f7d532318d82eef75fa13e7bb4f8b2c63
Francesco Trotta <fratrotta@gmail.com>
2017-04-02 18:49:52 +0200

    Ampliati Extraction test

--- src/test/scala/observatory/ExtractionTest.scala
+++ src/test/scala/observatory/ExtractionTest.scala
@@ -11,12 +11,12 @@ class ExtractionTest extends FunSuite {
 
   test("locateTemperatures") {
     import java.time.LocalDate
-    val  r = Extraction.locateTemperatures(2015, "/stations-test.csv.txt", "/2015-test.csv.txt")
-    assert( r.toList ===
+    val r = Extraction.locateTemperatures(2015, "/stations-test.csv.txt", "/2015-test.csv.txt")
+    assert(r.toList ===
       List(
-        (LocalDate.of(2015, 12, 25),Location(17.3,-5.23),0.0),
-        (LocalDate.of(2015, 12, 25),Location(20.0,20.0),100.0)
-    ))
+        (LocalDate.of(2015, 12, 25), Location(17.3, -5.23), 0.0),
+        (LocalDate.of(2015, 12, 25), Location(20.0, 20.0), 100.0)
+      ))
   }
 
   test("locationYearlyAverageRecords") {
@@ -25,24 +25,62 @@ class ExtractionTest extends FunSuite {
 
     assert(r.toList ===
       List(
-        (Location(17.3,-5.23),0.0),
-        (Location(20.0,20.0),100.0)
+        (Location(17.3, -5.23), 0.0),
+        (Location(20.0, 20.0), 100.0)
       )
     )
   }
 
-  /*test("locateTemperatures on real data") {
-    val start = System.currentTimeMillis()
-    val  r = Extraction.locateTemperatures(2015, "/stations.csv", "/2015.csv")
-    val stop = System.currentTimeMillis()
-    println(s"Processing locateTemperatures on real data took ${stop - start} ms.\n")
-  }*/
+  object SingleTempRecordsOrdering extends Ordering[(LocalDate, Location, Double)] {
+    def compare(a: (LocalDate, Location, Double), b: (LocalDate, Location, Double)) = {
+      if (a._1.isBefore(b._1)) -1
+      else if (a._1.isAfter(b._1)) 1
+      else if (a._2.lat < b._2.lat) -1
+      else if (a._2.lat > b._2.lat) 1
+      else (b._2.lon - a._2.lon).toInt
+    }
+  }
 
-  /*test("locationYearlyAverageRecords on real data") {
+  test("locateTemperatures on real data") {
+    val start = System.currentTimeMillis()
+    val r = Extraction.locateTemperatures(2015, "/stations.csv", "/2015.csv")
+    val stop = System.currentTimeMillis()
+    println(s"***\tProcessing locateTemperatures on real data took ${stop - start} ms.")
+
+    val temp = r.toList.sorted(SingleTempRecordsOrdering).take(4)
+    assert(temp === List(
+      (LocalDate.of(2015, 1, 1), Location(-90.0, 0.0), -8.666666666666668),
+      (LocalDate.of(2015, 1, 1), Location(-89.0, 89.667), -22.11111111111111),
+      (LocalDate.of(2015, 1, 1), Location(-89.0, -1.017), -22.27777777777778),
+      (LocalDate.of(2015, 1, 1), Location(-84.6, -115.817), -7.388888888888889)
+    )
+    )
+
+  }
+
+  object LocatioAverageTempRecordsOrdering extends Ordering[(Location, Double)] {
+    def compare(a: (Location, Double), b: (Location, Double)) = {
+      if (a._2 < b._2) -1
+      else if (a._2 > b._2) 1
+      else if (a._1.lat < b._1.lat) -1
+      else if (a._1.lat > b._1.lat) 1
+      else (b._1.lon - a._1.lon).toInt
+    }
+  }
+
+  test("locationYearlyAverageRecords on real data") {
     val start = System.currentTimeMillis()
     val records = Extraction.locateTemperatures(2015, "/stations.csv", "/2015.csv")
     val r = Extraction.locationYearlyAverageRecords(records)
     val stop = System.currentTimeMillis()
-    println(s"Processing locationYearlyAverageRecords on real data took ${stop - start} ms.\n")
-  }*/
+    println(s"***\tProcessing locationYearlyAverageRecords on real data took ${stop - start} ms.")
+    val temp = r.toList.sorted(LocatioAverageTempRecordsOrdering).take(4)
+    assert(temp === List(
+      (Location(40.839, 8.405), -17.959999999999997),
+      (Location(-89.0, -1.017), -17.511904761904763),
+      (Location(38.767, -104.3), -17.25757575757576),
+      (Location(15.233, -12.167), -17.0)
+    )
+    )
+  }
 }
